67e62086e4bc055d2b0fb0a94d73729008641124
Denno <dellyit001@gmail.com>
2025-08-29 23:51:33 +0300

    new commit

--- PixelSolution/obj/Debug/net8.0/PixelSolution.AssemblyInfo.cs
+++ PixelSolution/obj/Debug/net8.0/PixelSolution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1613ceac803c23b4cc1eedb4734950230b18c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541e4f16c37bb333c8dc5e4988d20dd8d58dfc83")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
